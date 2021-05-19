032f65798db72f2d39a85e2f67c65d6b2ade8783
Bunnilicious <51916275+Bunnilicious@users.noreply.github.com>
2021-05-19 19:00:42 +0300

    new site

---
+++ script/script.js
@@ -0,0 +1,8 @@
+$('.arparallax').arparallax({
+    'blur': 'auto', // нет, оба, вверх, вниз, авто
+    'depth': 'direct', // прямой, обратный, нет
+    'speed': .32, // 0 - 1
+    'scaleMultiplier': .05,
+    'blurMultiplier': 1,
+    'imageContainer': '.image'
+    });
